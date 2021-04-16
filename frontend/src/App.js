6dc0dedfa3f86bc13bf183fda93f9c8cea57a544
paul <www.cashisv2@gmail.com>
2021-04-16 18:16:40 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import data from "./data";
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a href="/" className="branding">
+            E-Commerce
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <div className="card" key={product._id}>
+              <a href={`/product/${product._id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
+              <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">&#8377;{product.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer className="row center">All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
