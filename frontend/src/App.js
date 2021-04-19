2b6e24142c81902df6ba142e85ad74a1a1a0845d
paul <www.cashisv2@gmail.com>
2021-04-19 10:34:06 +0530

    Added Product Screen Home Screen Rating Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,29 @@
-import data from "./data";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a href="/" className="branding">
-            E-Commerce
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div className="card" key={product._id}>
-              <a href={`/product/${product._id}`}>
-                <img
-                  className="medium"
-                  src={product.image}
-                  alt={product.name}
-                />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">&#8377;{product.price}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All Rights Reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a href="/" className="branding">
+              E-Commerce
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All Rights Reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Rating from "./Rating";
+
+export default function Product(props) {
+  const { product } = props;
+
+  return (
+    <div className="card" key={product._id}>
+      <a href={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt={product.name} />
+      </a>
+      <div className="card-body">
+        <a href={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </a>
+        <Rating
+          rating={product.rating}
+          numReviews={product.numReviews}
+        ></Rating>
+        <div className="price">&#8377;{product.price}</div>
+      </div>
+    </div>
+  );
+}
